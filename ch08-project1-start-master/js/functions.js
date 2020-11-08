da6fdee505557686ed5868e25f00f97e11cdc603
alekyalokasani <59576868+alekyalokasani@users.noreply.github.com>
2020-11-07 20:05:24 -0500

    Added functions Calculate total output cart row

--- ch08-project1-start-master/js/functions.js
+++ ch08-project1-start-master/js/functions.js
@@ -1,4 +1,25 @@
 /* define functions here */
 
 
+function calculateTotal(quantity,price)
+
+{
+
+    //return multiplication of quantity into price
+
+    return quantity*price;
+
+}
+
+//function to output cart row
+
+function outputCartRow(file,title,quantity,price,total)
+
+{
+
+    //create row
+
+    document.write("<tr><td><img src='"+file+"'></td><td>"+title+"</td><td>"+quantity+"</td><td>$"+price.toFixed(2)+"</td><td>$"+total.toFixed(2)+"</td></tr>");
+
+}
         
